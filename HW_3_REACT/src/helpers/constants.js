4758fb1becfef5a9ee5e41d50db47a28c1185482
AntonStognij <antonstognij5@gmail.com>
2022-09-04 17:37:07 +0300

    1.0.5

--- HW_3_REACT/src/helpers/constants.js
+++
@@ -1,7 +0,0 @@
-import photo from "assets/avatar.png"
-const defaultUser = {
-    sprites: {
-        front_default: photo
-    }
-}
-export {defaultUser}
--- HW_3_REACT/src/store/context.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const ContextUser = React.createContext()
-const ContextTask = React.createContext()
-
-export { ContextUser, ContextTask }
